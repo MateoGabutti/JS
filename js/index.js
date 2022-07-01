d9fbc06ef794e5a12056ae45264094c9ed937222
Mateo Gabutti <mateogabutti12@gmail.com>
2022-07-01 12:48:05 -0300

    Agregando funciones

--- js/index.js
+++ js/index.js
@@ -1,51 +1,70 @@
-/*let desayuno = prompt("Soles desayunar?Escribe 'si' o 'no' ").toLowerCase()
-if(desayuno == "si"){
-    console.log("Muy bien desayunando")
-}else{
-    console.log("TENES QUE DESAYUNAR")
-}
 
 
-if(numero < 10){
-    alert("Pusiste un numero menor a 10")
-}*/
-let edad = 18
+
 
 do{
     edad = parseInt(prompt("Ingrese su edad"))
-
     if(edad >= 18){
         alert("Usted es mayor de edad, puede ingresar")
     }else{
         alert("Usted es menor de edad, no puede ingresar")
     }
-}while(edad < 18)
+}while(edad == "" || edad < 18)
 
-/* OTRO EJEMPLO */
-
-let numero = parseInt(prompt("Ingrese una nota del 1 al 10 (si lo haces bien se te suma un punto en la nota, sino perdes la oportunidad)"))
-if(numero < 1 || numero > 10){
-    alert("Perdiste la oportunidad de sumar un punto")
-}else{
-    for(i = 1; i <= 1; i++ ){
-        alert(numero + i)
-        if(numero >= 6 && numero <= 9){
-            alert("Aprobaste")
-            
-        }else if(numero === 10){
-            alert("SUPERASTE LA NOTA MAXIMA, FELICITACIONES")
-            
+function fechaCumple(){
+    do{
+        cumpleaniosDia = parseInt(prompt("Como usted es mayor de edad, queremos saber que dia cumple (el numero, NO el mes)"))
+        cumpleaniosMes = prompt("Ahora queremos saber en que mes")
+        if(cumpleaniosDia < 1 || cumpleaniosDia > 31){
+            alert("Ingrese una fecha valida")
         }else{
-            alert("Desaprobaste")
-            
+            return `Cumplis el ${cumpleaniosDia} de ${cumpleaniosMes}. Estas habilitado para seguir con el cuestionario`
         }
-    }
-    console.log("Tu nota final es " + (numero + 1)) 
+    }while(cumpleaniosDia < 1 || cumpleaniosDia > 31 )
+    
 }
+let cumple = fechaCumple()
+alert(cumple)
+
+function saludar(nombre){
+    
+    nombre = prompt("Ahora, necesitamos saber su nombre")
+    return `Hola ${nombre}, como andas?`
+}
+let name = saludar()
+alert(name) 
+
+let desayuno = prompt("Que soles tomar en el desayuno?").toLowerCase()
+let desayunoComida = prompt("Que soles comer en el desayuno?").toLowerCase()
+
+function breakfast(){
+    if((desayuno === "nada" ||  desayuno === "")  || (desayunoComida === "nada" ||  desayunoComida === "")){
+        alert("No desayunas como corresponde")
+    }else{
+        return `${desayuno} y ${desayunoComida} que rico!`
+    }
+    
+}
+let sayuno = breakfast()
+alert(sayuno)
 
 
-/* OTRO EJEMPLO */
 
+/*function LocalDeRopa (nombreLocal = "Fika" , remeras, pantalones ){
+    this.nombreLocal = nombreLocal
+    this.remeras = remeras
+    this.pantalones = pantalones
+}
+let nombreLocal = prompt("Escribe el nombre de local")
+let remeras = prompt ("Que remera queres llevarte?")
+let pantalones = prompt ("Que pantalon queres llevarte?")
 
-
-
+const cliente = new LocalDeRopa(nombreLocal, remeras, pantalones)
+console.log(cliente)
+function Persona (nombre, apellido, edad){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.edad = edad
+}
+let nombre = prompt("Escriba su nombre")
+const persona1 = new Persona (nombre, apellido, edad)*/
