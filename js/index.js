d5f521257278489a37c4850fee1da35a5a3468aa
Mateo Gabutti <mateogabutti12@gmail.com>
2022-07-04 12:02:07 -0300

    Primer desafio entregable

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 
 
-
-
 do{
     edad = parseInt(prompt("Ingrese su edad"))
     if(edad >= 18){
@@ -18,36 +16,72 @@ function fechaCumple(){
         if(cumpleaniosDia < 1 || cumpleaniosDia > 31){
             alert("Ingrese una fecha valida")
         }else{
-            return `Cumplis el ${cumpleaniosDia} de ${cumpleaniosMes}. Estas habilitado para seguir con el cuestionario`
+            alert(`Cumplis el ${cumpleaniosDia} de ${cumpleaniosMes}. Estas habilitado para seguir con el cuestionario`)
+
         }
-    }while(cumpleaniosDia < 1 || cumpleaniosDia > 31 )
+        
+    }while(cumpleaniosDia < 1 || cumpleaniosDia > 31)
     
 }
 let cumple = fechaCumple()
-alert(cumple)
-
-function saludar(nombre){
-    
-    nombre = prompt("Ahora, necesitamos saber su nombre")
-    return `Hola ${nombre}, como andas?`
-}
-let name = saludar()
-alert(name) 
-
-let desayuno = prompt("Que soles tomar en el desayuno?").toLowerCase()
-let desayunoComida = prompt("Que soles comer en el desayuno?").toLowerCase()
 
 function breakfast(){
+    let desayuno = prompt("Que soles tomar en el desayuno?").toLowerCase()
+    let desayunoComida = prompt("Que soles comer en el desayuno?").toLowerCase()
+
     if((desayuno === "nada" ||  desayuno === "")  || (desayunoComida === "nada" ||  desayunoComida === "")){
         alert("No desayunas como corresponde")
     }else{
-        return `${desayuno} y ${desayunoComida} que rico!`
+        alert(`${desayuno} y ${desayunoComida} que rico!`)
     }
     
 }
 let sayuno = breakfast()
-alert(sayuno)
 
+
+alert("A continuacion, tenemos un acertijo para hacer")
+
+//adivina la palabra
+let palabra1 = "riquelme"
+let palabra2 = "paredes"
+let vidasParaAdivinar1 = 5
+let vidasParaAdivinar2 = 5
+
+function adivinaLaPalabra1(){
+    do{
+        palabraParaAdivinar = prompt(`Este es un acertijo. Tenes que adivinar el jugador de futbol (ESCRIBI EL APELLIDO). Es argentino, derecho y es considerado uno de los mejores jugadores del futbol argentino (YA ESTA RETIRADO). Tenes ${vidasParaAdivinar1} vidas`).toLowerCase()
+        if(palabraParaAdivinar == palabra1){
+            alert(`Felicitaciones, el jugador era Juan Roman Riquelme. Te quedaron ${vidasParaAdivinar1} vidas`)
+            break
+            
+        }else {
+            vidasParaAdivinar1 = vidasParaAdivinar1 - 1
+            if(vidasParaAdivinar1 <= 0){
+                alert(`Lo siento, te quedaste sin vidas el jugador era ${palabra1}`)
+                break
+            }
+        }
+    }while((palabraParaAdivinar !== palabra1) || (vidasParaAdivinar1 <= 0))
+}
+let acertijo = adivinaLaPalabra1()
+
+function adivinaLaPalabra2(){
+    do{
+        palabraParaAdivinar2 = prompt(`Otro acertijo. Tenes que adivinar el jugador de futbol(ESCRIBI EL APELLIDO). Es argentino, su pie habil es el derecho, es uno de los jugadores que potencio Scaloni en la seleccion y juega en la mitad de cancha. Tiene muchas chances de ir a Qatar  . Tenes ${vidasParaAdivinar2} vidas`).toLowerCase()
+        if(palabraParaAdivinar2 == palabra2){
+            alert(`Felicitaciones, el jugador era Paredes. Te quedaron ${vidasParaAdivinar2} vidas`)
+            break
+            
+        }else {
+            vidasParaAdivinar2 = vidasParaAdivinar2 - 1
+            if(vidasParaAdivinar2 <= 0){
+                alert(`Lo siento, te quedaste sin vidas el jugador era ${palabra2}`)
+                break
+            }
+        }
+    }while((palabraParaAdivinar !== palabra2) || (vidasParaAdivinar2 <= 0))
+}
+let acertijo2 = adivinaLaPalabra2()
 
 
 /*function LocalDeRopa (nombreLocal = "Fika" , remeras, pantalones ){
